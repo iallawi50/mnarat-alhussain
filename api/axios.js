8060ea5b36a8f6a94443f0480ba191a832c67bc8
Ali Al-Abdullah <aahh50018@gmail.com>
2023-07-13 16:15:57 +0300

    gg

--- api/axios.js
+++ api/axios.js
@@ -1,26 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+export default axios.create({
   baseURL: "https://manarat.svod.app/",
   withCredentials: true,
 });
-// استدعاء نقطة نهاية CSRF للحصول على الرمز
-const getCsrfToken = async () => {
-  try {
-    const response = await api.get("/sanctum/csrf-cookie");
-    const csrfToken = response.data.csrfToken;
-    return csrfToken;
-  } catch (error) {
-    console.error("Failed to fetch CSRF token");
-    throw error;
-  }
-};
-
-// تعيين رمز CSRF في رأس الطلبات
-api.interceptors.request.use(async (config) => {
-  const csrfToken = await getCsrfToken();
-  config.headers["X-CSRF-Token"] = csrfToken;
-  return config;
-});
-
-export default api;
